ba7c6a0ee1c2f549ce98d3d5acf6204b5f2fc255
Mahmoud Maher <mahmoudmaherofficial@gmail.com>
2024-12-25 02:09:14 +0200

    public api

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,5 +1,6 @@
 // Main Api Url
-export const baseUrl = 'http://127.0.0.1:8000/api';
+// export const baseUrl = 'http://127.0.0.1:8000/api';
+export const baseUrl = 'https://ecommerce-backend-production-0357.up.railway.app/api';
 
 // Auth
 export const RegisterUrl = `${baseUrl}/register`;
